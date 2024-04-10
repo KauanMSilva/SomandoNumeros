c7ce96857808b5a003274b8695907ac4cb78d5a7
Kauan Mariano Silva <144391114+KauanMSilva@users.noreply.github.com>
2024-04-09 21:26:44 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,15 @@
+// Primeiro programa que soma números
+public class Soma {
+
+// Você pelo cmd precisa colocar os números;
+public static void main(String args[]) {
+   int numero1 = Integer.parseInt(args[0]);
+   int numero2 = Integer.parseInt(args[1]);
+   int soma = numero1 + numero2;
+
+   System.out.println("A soma: " + soma);
+
+}
+
+
+}
